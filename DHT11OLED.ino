e9818b6814e8ba164214f13e5b0c492e2b5650aa
phuwanut2002 <34002935+phuwanut2002@users.noreply.github.com>
2021-04-23 22:26:33 +0700

    Add files via upload
    Now blinks and inverts screen only when there's no response from the sensor.
    Now have averages
    New initialization screen

--- DHT11OLED.ino
+++ DHT11OLED.ino
@@ -1,68 +1,71 @@
-//GFX
+//-----------------------------------------------------------------------------------OLED
+//OLED REFERENCES
+//FONT SIZE: (x,y) Height: 1x text 7px | 2x Text 14px | 3x text 21px
+
+//I2C protocol default SCL,SDA Pins
+//NodeMCU 1.0               SCL: D1/GPIO5    SDA: D2/GPIO4
+//Arduino UNO or Pro Mini   SCL: A5          SDA: A4
+//ESP32 DevKit V1 36 Pin    SCL: D22/GPIO22  SDA: D21/GPIO21
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define SCREEN_WIDTH 128    // OLED display width, in pixels
+#define SCREEN_HEIGHT 64    // OLED display height, in pixels
+#define OLED_RESET     -1   // OLED reset pin # (or -1 if none or sharing board reset pin)
+#define SCREEN_ADDRESS 0x3C //See datasheet for Address
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-//NodeMCU 1.0 SCL:D1/5 or D3/0 SDA:D2/4 or D4/2
-//(x,y) Height: 1x text 7px | 2x Text 14px | 3x text 21px
-//DHT Delay without delay
-const int ledPin =  LED_BUILTIN;// the number of the LED pin
-int ledState = LOW;             // ledState used to set the LED
-unsigned long previousMillis = 0;  // will store last time LED was updated
-const long interval = 1000;        // interval at which to blink (milliseconds)
+bool invertDisplay = false;
 
-//DHT Read
+//------------------------------------------------------------------------------------DHT
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
-#define DHTPIN D5
-#define DHTTYPE    DHT11
+//define DHTTYPE DHT22  //Change if you are using DHT22
+#define DHTTYPE DHT11   //Set sensor type
+#define DHTPIN D5       //Set to pin where the DHT Sensor data is connected
 DHT_Unified dht(DHTPIN, DHTTYPE);
 uint32_t delayMS;
+float currentTemp;
+float currentRH;
+float averageTemp;
+float averageRH;
+unsigned long samples;
+String Tavg;
+String RHavg;
+String totalSamples;
 
-//Web
 
-//----------------------------------------------------------------------------------------------------------
+//Delay without delay
+const byte ledPin = D0;    // pin of LED
+const byte ledPin2 = LED_BUILTIN;
+const bool invertedLED = true;
+bool ledState = LOW;               // state to set LED to
+unsigned long previousMillis = 0;  // last time function was called
+const long interval = 1000;        // interval
 
-void DHTRead(void) {
-}
-//----------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 void SplashScreen(void) {
-  display.clearDisplay();
-  display.cp437(true);         // Use full 256 char 'Code Page 437' font
-  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.clearDisplay();      // Always clear before writing new screen
   display.setCursor(0, 0);     // Start at top-left corner
-
-  display.setTextSize(1);      // Normal 1:1 pixel scale
-  display.write("Text1X");
-
-  display.setCursor(0, 9);
-  display.setTextSize(2);      // Normal 1:1 pixel scale
-  display.write("Text2X");
-
-  display.setCursor(0, 24);
-  display.setTextSize(3);      // Normal 1:1 pixel scale
-  display.write("Text3X");
-  display.display();
+  display.setTextSize(2);      // Set text size
+  display.write("Phuwanut");
+  display.setCursor(0, 15);    // Set second text starting location
+  display.write("DHT OLED");
+  display.display();           // Display written text on the display
 }
 
-//----------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-void DHT_initialize(void) {
+void DHT_initialize(void) { //Mostly from Adafruit DHT_Unified_Sensor example
   dht.begin();
-  // Print temperature sensor details.
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
   Serial.println(F("------------------------------------"));
-  Serial.println(F("Temperature Sensor"));
+  Serial.println(F("Temperature Sensor")); // Print temperature sensor details.
   Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
   Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
   Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
@@ -70,9 +73,8 @@ void DHT_initialize(void) {
   Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
   Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
   Serial.println(F("------------------------------------"));
-  // Print humidity sensor details.
   dht.humidity().getSensor(&sensor);
-  Serial.println(F("Humidity Sensor"));
+  Serial.println(F("Humidity Sensor"));    // Print humidity sensor details.
   Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
   Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
   Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
@@ -80,15 +82,15 @@ void DHT_initialize(void) {
   Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
   Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
   Serial.println(F("------------------------------------"));
-  // Set delay between sensor readings based on sensor details.
-  delayMS = sensor.min_delay / 1000;
+  const unsigned long delayMS = sensor.min_delay / 1000;       // Set delay between sensor readings based on sensor details.
 }
 
-//----------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-void OLED_initialize(void) {
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) { //idk why but need this for display to function
+
+void OLED_initialize(void) { // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) { // Idk why but need this for display to function
     Serial.println(F("SSD1306 allocation failed"));
     for (;;); // Don't proceed, loop forever
   }
@@ -96,56 +98,57 @@ void OLED_initialize(void) {
   display.display();
 }
 
-//==========================================================================================================
-//==========================================================================================================
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 void setup() {
+  OLED_initialize();
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.setTextColor(SSD1306_WHITE); // Draw white text
   int MaxTemp = 0;
   int MinTemp = 0;
   int MaxRH = 0;
   int MinRH = 0;
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
   Serial.begin(9600);
-  digitalWrite(ledPin, ledState);
+  digitalWrite(ledPin, invertedLED);
   //DHT
   DHT_initialize();
 
-  //OLED
-  OLED_initialize();
-
   SplashScreen();
-
+  if (invertedLED == true){
+    ledState = !ledState;
+  }
 }
+
+//=========================================================================================================================================================================================
+//=========================================================================================================================================================================================
 
 void loop() {
   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
-    if (ledState == LOW) {
-      ledState = HIGH;
-      analogWrite(ledPin, 1000);
-    }
-    else {
-      ledState = LOW;
-      digitalWrite(ledPin, HIGH);
-    }
+  if (currentMillis - previousMillis >= interval) { // Past the sensor interval
+    previousMillis = currentMillis;        // Save last time function was called
 
-    display.clearDisplay();
-    display.cp437(true);         // Use full 256 char 'Code Page 437' font
-    display.setTextColor(SSD1306_WHITE); // Draw white text
-    display.setCursor(0, 0);     // Start at top-left corner
-    display.setTextSize(3);
-    display.write("DHT11");
+    //digitalWrite(ledPin, ledState);        // Write LED state
+    //digitalWrite(ledPin2, !ledState);
+    display.clearDisplay();                // Always clear old stuff before drawing new stuff
+    display.setTextColor(SSD1306_WHITE);   // Draw white text
 
-    display.setTextSize(2);
+    display.setCursor(0, 0);               // Start at top-left corner
+    display.setTextSize(2);                // 2x font
+    display.write("DHT11");                // Actual text
 
-    display.setCursor(0, 24);
-    display.write("Temp:");
+    display.setCursor(0, 16);              // Move cursor Down
+    display.write("Temp:");                // Actual text 2
 
-    sensors_event_t event;
+    sensors_event_t event;                 // Read temperature data
+
     dht.temperature().getEvent(&event);
-    if (isnan(event.temperature)) {
+    if (isnan(event.temperature)) {        // If temperature is not a number
       Serial.println(F("Error reading temperature!"));
       display.write("Error");
     }
@@ -153,20 +156,16 @@ void loop() {
       Serial.print(F("Temperature: "));
       Serial.print(event.temperature);
       Serial.println(F("°C"));
-
+      currentTemp = event.temperature;
       String Temp = String(event.temperature, 1);
       display.write(Temp.c_str());
       display.write("C");
     }
-
-
-    // Get humidity event and print its value.
-
-    display.setCursor(0, 41);
+    display.setCursor(0, 31);
     display.write("RH:");
 
-    dht.humidity().getEvent(&event);
-    if (isnan(event.relative_humidity)) {
+    dht.humidity().getEvent(&event);      // Read relative humidity data
+    if (isnan(event.relative_humidity)) { // If relative humidity is not a number
       Serial.println(F("Error reading humidity!"));
       display.write("Error");
     }
@@ -174,13 +173,53 @@ void loop() {
       Serial.print(F("Humidity: "));
       Serial.print(event.relative_humidity);
       Serial.println(F("%"));
+      currentRH = event.relative_humidity;
       String RH = String(event.relative_humidity, 1);
-      char* buf2 = (char*) malloc(sizeof(char) * RH.length() + 1);
-      RH.toCharArray(buf2, RH.length() + 1);
-      display.write(buf2);
-      free(buf2);
+      display.write(RH.c_str());
       display.write("%");
     }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    ledState = !ledState; // Toggle LED state
+    if (isnan(event.temperature) || isnan(event.relative_humidity)) { // Not getting any data from DHT sensor
+      invertDisplay = !invertDisplay;
+      if (ledState == HIGH) {
+        digitalWrite(ledPin, ledState); // if invertedLED is true, LED turns off and vice versa
+        digitalWrite(ledPin2, ledState);
+      }
+      else if (ledState == LOW) {
+        digitalWrite(ledPin, ledState);
+        digitalWrite(ledPin2, ledState);
+      }
+    }
+    else if (!isnan(event.temperature) && !isnan(event.relative_humidity)) {
+      invertDisplay = false;
+      digitalWrite(ledPin, invertedLED);
+      digitalWrite(ledPin2, invertedLED);
+
+      samples++;
+      averageTemp += (currentTemp - averageTemp) / samples;
+      averageRH += (currentRH - averageRH) / samples;
+      Serial.print("averageTemp: ");
+      Serial.println(averageTemp);
+      Serial.print("averageRH: ");
+      Serial.println(averageRH);
+      Tavg = String(averageTemp, 1);
+      RHavg = String(averageRH, 1);
+      totalSamples = String(samples);
+    }
+    display.setTextSize(1);
+    display.setCursor(0, 47);
+    display.write("Tavg:");
+    display.write(Tavg.c_str());
+    display.setCursor(64, 47);
+    display.write("RHavg:");
+    display.write(RHavg.c_str());
+    display.setCursor(0, 57);
+    display.write("count:");
+    display.write(totalSamples.c_str());
+    display.invertDisplay(invertDisplay);
     display.display();
   }
 }
